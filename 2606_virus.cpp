e20ccbea02eae36fb3334bd487a0427a196e15ed
JeonJeongMin <35222185+JeonJeongMin@users.noreply.github.com>
2018-04-16 21:29:10 +0900

    2606.virus
    2606.virus

---
+++ 2606_virus.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+#include<string>
+#include<vector>
+#include<malloc.h>
+using namespace std;
+#define MAX_V 101
+int graph[MAX_V][MAX_V];
+int visit[MAX_V];
+int v_n, edge_n;
+int num_virus=-1;
+
+void dfs(int v){
+	visit[v]=1;
+	num_virus++;
+	for(int i=1;i<=v_n;i++){
+		if(!visit[i]&&graph[v][i]){
+			dfs(i);
+		}
+	}
+	
+}
+
+int main(){
+	
+	cin>>v_n>>edge_n;
+	int v, w;
+	for(int i=0;i<=v_n;i++){
+		for(int j=0;j<=v_n;j++){
+			graph[i][j]=0;
+		}
+		visit[i]=0;
+	}
+
+	
+	for(int i=1;i<=edge_n;i++){
+		cin>>v>>w;
+		graph[v][w]=graph[w][v]=1;
+	}
+	
+	dfs(1);
+	cout<<num_virus<<endl;
+	
+	return 0;
+}
+
